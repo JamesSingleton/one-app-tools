e595ae1e5b93069aba519dd8a202726d77c61f6a
James Singleton <jamesrsingleton1@gmail.com>
2020-07-18 00:19:51 -0700

    chore(examples): add more examples

---
+++ examples/basic-child-module/src/components/BasicChildModule.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const BasicChildModule = () => {
+  <Fragment>
+    <h1>Welcome to Basic Child Module</h1>
+  </Fragment>;
+};
+
+export default BasicChildModule;
