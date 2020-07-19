f90bc0ae278403da389309476c03c5a4d77f3493
James Singleton <jamesrsingleton1@gmail.com>
2020-07-18 20:35:07 -0700

    chore(examples): update root w/react-intl example
    This fixes the example so the root module actually works now.

--- examples/root-module-with-react-intl/src/components/RootModuleWithReactIntl.jsx
+++ examples/root-module-with-react-intl/src/components/RootModuleWithReactIntl.jsx
@@ -1,42 +1,48 @@
 import React from 'react';
-import {
-  FormattedMessage,
-  FormattedNumber,
-  defineMessages,
-  useIntl,
-  IntlProvider,
-} from 'react-intl';
+import PropTypes from 'prop-types';
+import { FormattedMessage, FormattedNumber, IntlProvider } from 'react-intl';
 import { loadLanguagePack } from '@americanexpress/one-app-ducks';
 import childRoutes from '../childRoutes';
 
-const { description } = defineMessages({
-  description: {
-    id: 'description',
-    defaultMessage: 'An example module using React Intl with One App.',
-  },
-});
+const RootModuleWithReactIntl = ({ languageData, localeName }) => (
+  <IntlProvider locale={localeName} messages={languageData}>
+    <p>
+      <FormattedMessage id="greeting" defaultMessage="Hello, World!" />
+    </p>
+    <p>
+      <FormattedNumber value={9000} />
+    </p>
+  </IntlProvider>
+);
 
-const RootModuleWithReactIntl = ({ languageData, localeName }) => {
-  const intl = useIntl();
-  return (
-    <IntlProvider locale={localeName} messages={languageData}>
-      <p>
-        <FormattedMessage id="greeting" defaultMessage="Hello, World!" />
-      </p>
-      <p>
-        <FormattedNumber value={9000} />
-      </p>
-      <p>
-        <span>{intl.formatMessage(description)}</span>
-      </p>
-    </IntlProvider>
+export const loadModuleData = async ({ store: { dispatch } }) => {
+  await dispatch(
+    loadLanguagePack('root-module-with-react-intl', { fallbackLocale: 'en-US' })
   );
 };
 
-export const loadModuleData = ({ store: { dispatch } }) =>
-  dispatch(
-    loadLanguagePack('root-module-with-react-intl', { fallbackLocale: 'en-US' })
-  );
+export const mapStateToProps = (state) => {
+  const localeName = state.getIn(['intl', 'activeLocale']);
+  const languagePack = state
+    .getIn(
+      ['intl', 'languagePacks', localeName, 'root-module-with-react-intl'],
+      fromJS({})
+    )
+    .toJS();
+
+  return {
+    languageData: languagePack && languagePack.data ? languagePack.data : {},
+    localeName,
+  };
+};
+
+RootModuleWithReactIntl.propTypes = {
+  languageData: PropTypes.shape({
+    greeting: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
+  localeName: PropTypes.string.isRequired,
+};
 
 RootModuleWithReactIntl.holocron = {
   name: 'root-module-with-react-intl',
@@ -50,4 +56,4 @@ if (!global.BROWSER) {
   RootModuleWithReactIntl.appConfig = require('../appConfig').default;
 }
 
-export default RootModuleWithReactIntl;
+export default connect(mapStateToProps)(RootModuleWithReactIntl);
